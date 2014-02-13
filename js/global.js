be703cfd42ed6f75087df7be4b1af1286dbb86ba
John Meyers <meyersjg@miamioh.edu>
2014-02-13 14:46:23 -0600

    Added js to remove days for mobile

--- js/global.js
+++ js/global.js
@@ -2,3 +2,29 @@
 $(function () {
 
 });
+
+
+
+
+function RemoveDays() {
+  $(".day").each(function(index) {
+    if (!$(this).hasClass("event")) {
+      $(this).css("display", "none");
+    }
+  });
+}
+
+function AddDays() {
+  $(".day").each(function(index) {
+    $(this).css("display", "table-cell");
+  });
+}
+
+$(window).resize(function(){
+  if ($(".day").css("float") == "left" ){
+    RemoveDays();
+  } else {
+    AddDays()
+  }
+
+});
